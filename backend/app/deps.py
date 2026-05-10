d13a51fd3183076b37da2a32914538c204044df7
KaranSJ22 <karanjamkhandi@gmail.com>
2026-05-10 23:20:05 +0530

    feat:lender-models-routes

--- backend/app/deps.py
+++ backend/app/deps.py
@@ -1,18 +1,3 @@
-# from collections.abc import Generator
-
-# from sqlalchemy.orm import Session
-
-# from .database import SessionLocal
-
-
-# def get_db() -> Generator[Session, None, None]:
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
 from collections.abc import Generator
 
 from fastapi import Depends, HTTPException, status
